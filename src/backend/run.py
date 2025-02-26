695c9d75fbd87faad4133fcea25a8b9a8169acf4
asewnandan <a.sewnandan@hhs.nl>
2025-02-26 02:39:18 +0100

    Update run.py

--- src/backend/run.py
+++ src/backend/run.py
@@ -1,4 +1,5 @@
 from extractor import find_tables_from_file
+import os
 
 # Import File Overview
 
@@ -6,11 +7,18 @@ from extractor import find_tables_from_file
 
 # ---
 
-# Execute extractor()
+# Execute extractor() 
+# --- TEMPORARY --- 
+# Finds all tables in a given directory and saves them to a JSON file
+def find_and_process_files(root_path):
+    for root, dirs, files in os.walk(root_path):
+        for file in files:
+            if "Bestandsbeschrijving" in file and file.endswith(".txt"):
+                full_path = os.path.join(root, file)
+                find_tables_from_file(full_path)
 
-file_paths = [path_1cho,path_croho, path_croho_vest, path_dec, path_vakkenbestand]
-for path in file_paths:
-    find_tables_from_file(path)
+root_directory = "data/01-raw/DUO/1CHO/2025/"
+find_and_process_files(root_directory)
 
 # Execute converter()
 
